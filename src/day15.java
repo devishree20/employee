d148280737605776903a01d4f6409a3db0e5636e
devishree <devishreee25csc@vetias.ac.in>
2026-06-23 11:11:55 +0530

    day15

--- src/day15.java
+++ src/day15.java
@@ -1,21 +1,52 @@
-
-public class day15 {
+/*public class day15 {
     static void main() throws InterruptedException {
         Runnable chefTask = () -> {
-            String[] items = {"sweet", "rice", "dosa"};
+            String[] items = {"Bun", "Biscuit", "tea"};
             for (int i = 0; i < items.length; i++) {
-                System.out.println("Cooking Item:" + items[i]);
+                System.out.println("Cooking item: " + items[i]);
                 try {
-                    Thread.sleep(7000);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
-
         };
-        Thread chefThread = new Thread(chefTask, "chef_thread");
+        Thread chefThread = new Thread(chefTask, "chef-task");
         chefThread.start();
         chefThread.join();
+    }
+}*/
+
+public class day15 {
+    static String[] items = {"frenchfries","brownie","milkshake"};
+
+    private static int nextOrderIndex = 0;
+
+    private static final Object LOCK = new Object();
+
+    static void pickOrder(){
+        String order;
+        while(true){
+            synchronized(LOCK){
+                if(nextOrderIndex>=items.length){
+                    return;
+                }
+                order = items[nextOrderIndex];
+                nextOrderIndex++;
+            }
+            System.out.println(Thread.currentThread().getName()+" is preparing : "+order);
+        }
+    }
+
+    static void main() throws InterruptedException {
+        Thread chefOne = new Thread(()->pickOrder(), "chef-one");
+        Thread chefTwo = new Thread(()->pickOrder(), "chef-two");
+
+        chefOne.start();
+        chefTwo.start();
+
+        chefOne.join();
+        chefTwo.join();
 
     }
 }
